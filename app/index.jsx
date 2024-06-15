00deca39130a16d7a5d801ab27ceb3f572675b27
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2024-06-15 14:22:24 +0300

    tabs layout completed

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
     <View className="bg-red-400 border flex items-center">
       <Text className="text-3xl">Hello world dd</Text>
       <StatusBar style="auto" />
-      <Link className='bg-blue font-pblack' href='/profile'>dwdwd</Link>
+      <Link className='bg-blue font-pblack' href='/home'>homr</Link>
     </View>
   );
 }
