3854725092104e9b6a455d4b73b329f38b9e68db
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2024-06-22 15:17:37 +0300

    search(query) page completed

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,11 +1,11 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { View, TextInput, TouchableOpacity, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants";
-import { usePathname } from "expo-router";
+import { router, usePathname } from "expo-router";
 
-const SearchInput = ({initialQuery}) => {
-    const pathname = usePathname();
-const [query, setQuery] = useState(initialQuery || "");
+const SearchInput = ({ initialQuery }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || "");
 
   return (
     <View className="flex flex-row items-center space-x-4 w-full h-16 px-4 bg-black-100 rounded-2xl border-2 border-black-200 focus:border-secondary">
@@ -18,15 +18,18 @@ const [query, setQuery] = useState(initialQuery || "");
       />
 
       <TouchableOpacity
-      onPress={() => {
+        onPress={() => {
+          if (query === "")
+            return Alert.alert(
+              "Missing query ",
+              "Please input something to search results across database"
+            );
 
-      }}
+          if (pathname.startsWith("/search")) router.setParams({ query });
+          else router.push(`/search/${query}`);
+        }}
       >
-        <Image
-        source={icons.search}
-        className="w-5 h-5"
-        resizeMode="contain"
-        />
+        <Image source={icons.search} className="w-5 h-5" resizeMode="contain" />
       </TouchableOpacity>
     </View>
   );
